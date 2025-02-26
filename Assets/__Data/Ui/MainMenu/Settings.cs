2278475248aa48c7024349cc612e1116906cf2ec
DESKTOP-8O8JO1B\QunLee273 <quan2732k3@gmail.com>
2025-02-27 03:15:03 +0700

    setting main menu ( music)

---
+++ Assets/__Data/Ui/MainMenu/Settings.cs
@@ -0,0 +1,61 @@
+using __Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : GameBehaviour
+{
+    [SerializeField] protected Button btnMusic;
+    [SerializeField] protected Button btnBack;
+    
+    private bool isMusic = true;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        LoadBtnMusic();
+        LoadBtnBack();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        btnMusic.onClick.AddListener(OnClickMusic);
+        btnBack.onClick.AddListener(OnClickBack);
+    }
+
+    private void LoadBtnMusic()
+    {
+        if (btnMusic != null) return;
+        btnMusic = transform.Find("BtnMusic").GetComponent<Button>();
+        Debug.LogWarning(transform.name + ": LoadBtnMusic", gameObject);
+    }
+
+    private void LoadBtnBack()
+    {
+        if (btnBack != null) return;
+        btnBack = transform.Find("BtnBack").GetComponent<Button>();
+        Debug.LogWarning(transform.name + ": LoadBtnBack", gameObject);
+    }
+
+    private void OnClickMusic()
+    {
+        TMP_Text tmpText = GetComponentInChildren<TMP_Text>();
+        if (isMusic)
+        {
+            tmpText.text = "Music: Off";
+            isMusic = false;
+        }
+        else
+        {
+            tmpText.text = "Music: On";
+            isMusic = true;
+        }
+    }
+
+    private void OnClickBack()
+    {
+        UICtrl.Instance.SelectLevel.gameObject.SetActive(true);
+        UICtrl.Instance.Settings.gameObject.SetActive(false);
+    }
+}
